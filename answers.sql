25e5adef233e287482d46dbf0dc114e4b28a984f
Craig <craigthompson85@gmail.com>
2024-05-24 09:25:53 -0600

    Added answers.sql file and completed problem 1

---
+++ answers.sql
@@ -0,0 +1,19 @@
+--===================================================--
+--  Answers
+--===================================================--
+
+-------------------------------------------------------
+-- PROBLEM 1
+-------------------------------------------------------
+--  The Wasatch Cupcake Shop is getting ready to send 
+--    a marketing email announcing a new promotion on 
+--    Funfetti cupcakes. We need to get all of our 
+--    customers’ email addresses to Marketing.
+
+--  Select all of the email addresses from the customers 
+--    table in alphabetical order.
+SELECT email FROM customers ORDER BY email ASC;
+
+-------------------------------------------------------
+-- PROBLEM 2
+-------------------------------------------------------
