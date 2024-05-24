d876171c4009fd589573f6ea3d22c28e4ff247d5
Craig <craigthompson85@gmail.com>
2024-05-24 09:36:30 -0600

    Completed problem 2

--- answers.sql
+++ answers.sql
@@ -1,10 +1,10 @@
---===================================================--
+--=============================================================--
 --  Answers
---===================================================--
+--=============================================================--
 
--------------------------------------------------------
--- PROBLEM 1
--------------------------------------------------------
+-----------------------------------------------------------------
+--  PROBLEM 1
+-----------------------------------------------------------------
 --  The Wasatch Cupcake Shop is getting ready to send 
 --    a marketing email announcing a new promotion on 
 --    Funfetti cupcakes. We need to get all of our 
@@ -12,8 +12,24 @@
 
 --  Select all of the email addresses from the customers 
 --    table in alphabetical order.
-SELECT email FROM customers ORDER BY email ASC;
+SELECT email 
+FROM customers 
+ORDER BY email ASC;
 
--------------------------------------------------------
--- PROBLEM 2
--------------------------------------------------------
+-----------------------------------------------------------------
+--  PROBLEM 2
+-----------------------------------------------------------------
+--  Elizabeth Crocker has requested receipts for all of her past 
+--    cupcake orders. To look up the receipts, we need to know 
+--    which orders Elizabeth placed.
+
+--  Write a query that lists the order ids for all orders placed 
+--    by customers with the first name Elizabeth and last name 
+--    Crocker. Use a SUBQUERY to do this, not a join.
+SELECT id 
+FROM public.orders 
+  WHERE customer_id = 
+    (SELECT id 
+    FROM customers 
+		WHERE fname='Elizabeth' AND lname='Crocker');
+
